b29e5a2449ea784e3041c98822479c41a6b876fe
xucheng <3020271276@qq.com>
2025-09-02 15:17:45 +0800

    暂时放开AI模块的权限校验

--- xcoj-ai-service/src/main/java/com/github/cnxucheng/xcojaiservice/controller/MainController.java
+++ xcoj-ai-service/src/main/java/com/github/cnxucheng/xcojaiservice/controller/MainController.java
@@ -1,16 +1,11 @@
 package com.github.cnxucheng.xcojaiservice.controller;
 
-import com.github.cnxucheng.xcojModel.entity.User;
-import com.github.cnxucheng.xcojModel.enums.UserRoleEnum;
 import com.github.cnxucheng.xcojModel.vo.AIRequest;
 import com.github.cnxucheng.xcojaiservice.agent.DeepseekAgent;
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
-import static com.github.cnxucheng.common.constant.UserLoginState.USER_LOGIN_STATE;
 
 @RestController
 @RequestMapping("/ai")
