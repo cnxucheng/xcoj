aca7b72820d5344b7476793a03f2d8200adb1146
xucheng <3020271276@qq.com>
2025-09-02 15:17:17 +0800

    暂时放开AI模块的权限校验

--- xcoj-ai-service/src/main/java/com/github/cnxucheng/xcojaiservice/controller/MainController.java
+++ xcoj-ai-service/src/main/java/com/github/cnxucheng/xcojaiservice/controller/MainController.java
@@ -4,14 +4,13 @@ import com.github.cnxucheng.xcojModel.entity.User;
 import com.github.cnxucheng.xcojModel.enums.UserRoleEnum;
 import com.github.cnxucheng.xcojModel.vo.AIRequest;
 import com.github.cnxucheng.xcojaiservice.agent.DeepseekAgent;
-import com.github.cnxucheng.xcojfeignclient.service.UserFeignClient;
 import jakarta.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
-import java.util.Objects;
+import static com.github.cnxucheng.common.constant.UserLoginState.USER_LOGIN_STATE;
 
 @RestController
 @RequestMapping("/ai")
@@ -20,30 +19,13 @@ public class MainController {
     @Resource
     private DeepseekAgent deepseekAgent;
 
-    @Resource
-    private UserFeignClient userFeignClient;
-
     @PostMapping("/rag")
-    public Flux<String> rag(@RequestBody AIRequest aiRequest, HttpServletRequest request, HttpServletResponse response) {
-        User user = userFeignClient.getLoginUser(request);
-        if (Objects.equals(user.getUserRole(), UserRoleEnum.BAN.getValue()) ) {
-            response.setStatus(401);
-            return Flux.empty();
-        }
-        return deepseekAgent.doChatWithRag(
-                aiRequest.getMessage()+ "\n我的用户ID为" + user.getUserId(),
-                aiRequest.getChatId());
+    public Flux<String> rag(@RequestBody AIRequest aiRequest) {
+        return deepseekAgent.doChatWithRag(aiRequest.getMessage(), aiRequest.getChatId());
     }
 
     @PostMapping("/manus")
-    public Flux<String> manus(@RequestBody AIRequest aiRequest, HttpServletRequest request, HttpServletResponse response) {
-        User user = userFeignClient.getLoginUser(request);
-        if (Objects.equals(user.getUserRole(), UserRoleEnum.BAN.getValue()) ) {
-            response.setStatus(401);
-            return Flux.empty();
-        }
-        return deepseekAgent.doChatByManus(
-                aiRequest.getMessage() + "\n我的用户ID为" + user.getUserId(),
-                aiRequest.getChatId());
+    public Flux<String> manus(@RequestBody AIRequest aiRequest) {
+        return deepseekAgent.doChatByManus(aiRequest.getMessage(), aiRequest.getChatId());
     }
 }
